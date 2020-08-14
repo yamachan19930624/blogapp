fe6c8dd8194ebe7380dfce82d57af30728f63f30
山下優樹 <>
2020-08-14 19:10:05 +0900

    seedの修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,23 +1,41 @@
+
+
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  content    :text             not null
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_articles_on_user_id  (user_id)
+#
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :title, length: { minimum: 2, maximum: 100 }
-  validates :title, format: { with: /\A(?!\@)/}
-
+  validates :title, format: { with: /\A(?!\@)/ }
   validates :content, presence: true
   validates :content, length: { minimum: 10 }
   validates :content, uniqueness: true
-
   validate :validate_title_and_content_length
-
+  belongs_to :user
+  
   def display_created_at
     I18n.l(self.created_at, format: :default)
   end
 
+  def author_name
+    user.display_name
+  end
+
   private
-  def  validate_title_and_content_length
+  def validate_title_and_content_length
     char_count = self.title.length + self.content.length
-    unless char_count > 100
-      errors.add(content,'100文字以上でふ！')
-    end
+    errors.add(:content, '100文字以上は無理減らして！') unless char_count > 100
   end
 end
